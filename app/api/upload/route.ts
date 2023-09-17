8910a26d21d6c9c7cfe80cb94f386c812dd8705b
Othy Tenk <othy.tenk@gmail.com>
2023-09-17 17:00:25 +0800

    feat: upload file use client side and server side

---
+++ app/api/upload/route.ts
@@ -0,0 +1,21 @@
+import { writeFile } from "fs/promises";
+import { NextRequest, NextResponse } from "next/server";
+import { join } from "path";
+
+export async function POST(request: NextRequest) {
+    const data = await request.formData()
+    const file:File | null = data.get('file') as File
+
+    if (!file) {
+        return NextResponse.json({ success: false, message: 'No file uploaded' })
+    }
+
+    const bytes = await file.arrayBuffer()
+    const buffer = Buffer.from(bytes)
+    
+    const filePath = join('/', 'tmp', file.name)
+    await writeFile(filePath, buffer)
+    console.log('File saved to', filePath)
+
+    return NextResponse.json({ success: true, message: 'File uploaded' })
+}
