49b028cdcfc3a241bd4cecc90f01286ba5947657
Othy Tenk <othy.tenk@gmail.com>
2023-09-24 21:19:12 +0800

    feat: added file split

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,6 +1,12 @@
-import { writeFile } from "fs/promises";
+import { read } from "fs";
+import { readFile, writeFile } from "fs/promises";
 import { NextRequest, NextResponse } from "next/server";
 import { join } from "path";
+
+const writeLines = async (lines: string[], fileIndex: number) => {
+  const filePath = join(process.cwd(), "public", `file-${fileIndex}.txt`);
+  await writeFile(filePath, lines.join("\n"));
+};
 
 export async function POST(request: NextRequest) {
   const data = await request.formData();
@@ -14,8 +20,34 @@ export async function POST(request: NextRequest) {
   const buffer = Buffer.from(bytes);
 
   const filePath = join(process.cwd(), "public", file.name);
-  await writeFile(filePath, buffer);
+  const result = await writeFile(filePath, buffer);
   console.log("File saved to", filePath);
+
+  console.log("result ", result);
+
+  const allDatas = await readFile(filePath, {
+    encoding: "utf-8",
+  });
+
+  let fileIndex = 0;
+  let indexCount = 0;
+
+  let lines: string[] = [];
+
+  allDatas.split("\n").forEach((line, index) => {
+    lines.push(line);
+
+    if (indexCount === 999 || index === lines.length - 1) {
+      writeLines(lines, fileIndex);
+      fileIndex++;
+      indexCount = 0;
+      lines = [];
+    } else {
+      indexCount++;
+    }
+  });
+
+  console.log(`Used ${process.memoryUsage().heapUsed / 1024 / 1024} MB`);
 
   return NextResponse.json({ success: true, message: "File uploaded" });
 }
